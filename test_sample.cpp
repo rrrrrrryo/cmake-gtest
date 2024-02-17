e133ea60b220afd0680e609d3a2b9541228c33c5
Ryo Ota <ota.ryo.1026@gmail.com>
2024-02-17 17:10:46 +0900

    initial commit

---
+++ test_sample.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
